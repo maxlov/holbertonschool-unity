ccb70af075fc368d6ebb2b8b10972bd3a452564f
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-10-20 16:46:51 -0400

    [Create] Functionality for volume sliders

--- 0x08-unity-audio/Assets/Scripts/UI/OptionsMenu.cs
+++ 0x08-unity-audio/Assets/Scripts/UI/OptionsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
@@ -9,10 +10,14 @@ public class OptionsMenu : MonoBehaviour
     private GameManager gameManager;
 
     public Toggle InvertY;
+    public Slider SFXVol;
+    public Slider BGMVol;
 
     private int InvertYTemp;
     private float SFXVolumeTemp;
     private float BGMVolumeTemp;
+
+    public AudioMixer masterMixer;
 
     private void Start()
     {
@@ -21,6 +26,9 @@ public class OptionsMenu : MonoBehaviour
 
         InvertY.isOn = PlayerPrefs.GetInt("InvertedY") == 1 ? true : false;
         InvertYTemp = PlayerPrefs.GetInt("InvertedY");
+
+        SFXVol.value = Mathf.Pow(10.0f, PlayerPrefs.GetFloat("SFXvol", 0.0f) / 20.0f);
+        BGMVol.value = Mathf.Pow(10.0f, PlayerPrefs.GetFloat("BGMvol", 0.0f) / 20.0f);
     }
 
     public void Back()
@@ -31,6 +39,8 @@ public class OptionsMenu : MonoBehaviour
     public void Apply()
     {
         PlayerPrefs.SetInt("InvertedY", InvertYTemp);
+        PlayerPrefs.SetFloat("BGMvol", BGMVolumeTemp);
+        PlayerPrefs.SetFloat("SFXvol", SFXVolumeTemp);
         gameManager.LoadPrev();
     }
 
@@ -38,4 +48,17 @@ public class OptionsMenu : MonoBehaviour
     {
         InvertYTemp = InvertY.isOn ? 1 : 0;
     }
+
+    public void SetMusicVol()
+    {
+        BGMVolumeTemp = Mathf.Log10(BGMVol.value) * 20;
+        Debug.Log($"slidVal {BGMVol.value} BGMtempVol {BGMVolumeTemp}");
+    }
+
+    public void SetSFXVol()
+    {
+        SFXVolumeTemp = Mathf.Log10(SFXVol.value) * 20;
+    }
+
+
 }
---
+++ 0x08-unity-audio/Assets/Scripts/UI/VolumeManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    public static VolumeManager instance;
+
+    /*void Awake()
+    {
+        if (VolumeManager.instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            VolumeManager.instance = this;
+        }
+        else
+            Destroy(gameObject);
+    }*/
+    void Start()
+    {
+        //Get the saved music volume, standard = 10f
+        Debug.Log(PlayerPrefs.GetFloat("BGMvol", 0.0f));
+        float music = PlayerPrefs.GetFloat("BGMvol", 0.0f);
+        float master = PlayerPrefs.GetFloat("SFXvol", 0.0f);
+        //Set the music volume to the saved volume
+        AdjustMusicVolume(music);
+        AdjustSFXVolume(master);
+    }
+
+
+
+    public void AdjustMusicVolume(float volume)
+    {
+        //Update AudioMixer
+        audioMixer.SetFloat("BGMvol", volume);
+        //Update PlayerPrefs "Music"
+        PlayerPrefs.SetFloat("BGMvol", volume);
+        //Save changes
+        PlayerPrefs.Save();
+    }
+
+    public void AdjustSFXVolume(float volume)
+    {
+        //Update AudioMixer
+        audioMixer.SetFloat("Mastervol", volume);
+        //Update PlayerPrefs "Music"
+        PlayerPrefs.SetFloat("SFXvol", volume);
+        //Save changes
+        PlayerPrefs.Save();
+    }
+
+}
